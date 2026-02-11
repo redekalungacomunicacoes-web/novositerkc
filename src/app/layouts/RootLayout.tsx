6e4656844abc24660e9badd6c5a1231d68b6e6c5
redekalungacomunicacoes-web <redekalungacomunicacoes@gmail.com>
2026-02-11 09:52:16 -0300

    Update RootLayout.tsx

--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -1,10 +1,12 @@
-import { Outlet } from 'react-router-dom';
-import { Header } from '@/app/components/Header';
-import { Footer } from '@/app/components/Footer';
+import { Outlet } from "react-router-dom";
+import { Header } from "@/app/components/Header";
+import { Footer } from "@/app/components/Footer";
+import { ScrollToTop } from "@/app/components/ScrollToTop";
 
 export function RootLayout() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Header />
       <main className="flex-1">
         <Outlet />
