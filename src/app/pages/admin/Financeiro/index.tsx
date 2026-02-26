140bd57f47a3b8f8066e9e4a47b90855c1ea1ff9
Felipe Costa <playmomentsstudios@gmail.com>
2026-02-26 17:40:03 -0300

    Remove export for Relatorios from Financeiro

--- src/app/pages/admin/Financeiro/index.tsx
+++ src/app/pages/admin/Financeiro/index.tsx
@@ -3,5 +3,3 @@ export { Fundos } from './Fundos';
 export { FundoDetalhes } from './FundoDetalhes';
 export { Projetos } from './Projetos';
 export { ProjetoDetalhes } from './ProjetoDetalhes';
-
-export { Relatorios } from './Relatorios';
