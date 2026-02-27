de4119432a61802ba90df2ca8da44cc7b79086f2
Felipe Costa <playmomentsstudios@gmail.com>
2026-02-27 09:10:51 -0300

    Delete src/app/pages/admin/Financeiro directory

--- src/app/pages/admin/Financeiro/index.tsx
+++
@@ -1,5 +0,0 @@
-export { Dashboard } from './Dashboard';
-export { Fundos } from './Fundos';
-export { FundoDetalhes } from './FundoDetalhes';
-export { Projetos } from './Projetos';
-export { ProjetoDetalhes } from './ProjetoDetalhes';
